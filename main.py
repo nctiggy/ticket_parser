904affb2ab06e8273bbe7d67178dfd7483519121
Craig J Smith <nctiggy@gmail.com>
2018-12-11 18:48:10 -0800

    initial

---
+++ main.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import json
+import csv
+import sys
+import pprint
+
+file = sys.argv[1]
+out_file = sys.argv[2]
+
+data = []
+with open(file) as f:
+    for row in csv.DictReader(f):
+        data.append(row)
+
+tickets = json.loads(json.dumps(data))
+final_json = {'tickets': []}
+
+for ticket in tickets:
+    final_ticket = {}
+    # Pull out the description field
+    # Remove --- and convert \n to a comma
+    malformed_description = ticket["Description"].replace("---", "").strip()
+    description = malformed_description.replace("\n", ",").strip()
+    # Turn description into an array
+    items = description.split(",")
+    # Parse through each item in description and format it to proper json
+    for item in items:
+        element = item.split(":")
+        if len(element) == 2:
+            key = element[0]
+            early_value = element[1].strip("'")
+            value = early_value.strip("'")
+            # Add the well formed JSON into the final p
+            final_ticket.update({key: value})
+
+    # Delete description from the JSON object
+    ticket.pop('Description', None)
+    # Add original ticket minus description to the new json object
+    final_ticket.update(ticket)
+    final_json['tickets'].append(final_ticket)
+
+pprint.pprint(final_json)
+
+# Store records for later use
+records = []
+
+# Keep track of headers in a set
+headers = set([])
+
+for line in final_json['tickets']:
+    # line = line.strip()
+
+    # Parse each line as JSON
+    # parsedJson = json.loads(line)
+
+    records.append(line)
+
+    # Make sure all found headers are kept in the headers set
+    for header in line.keys():
+        headers.add(header)
+
+# You only know what headers were there once you have read all the JSON once.
+
+# Now we have all the information we need, like what all possible headers are.
+
+outfile = open(out_file, 'w')
+
+# write headers to the file in order
+outfile.write(",".join(sorted(headers)) + '\n')
+
+for record in records:
+    # write each record based on available fields
+    curLine = []
+    for header in sorted(headers):
+        if header in record:
+            curLine.append(record[header])
+        else:
+            curLine.append('')
+    outfile.write(",".join(curLine) + '\n')
+
+outfile.close()
